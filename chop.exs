1fa6a333394c272e81307eb9c955efdf3bd25775
mitsu9 <mitsunobu.h.9@gmail.com>
2017-07-04 20:58:55 +0900

    Fix

--- chop.exs
+++ chop.exs
@@ -1,20 +1,20 @@
 defmodule Chop do
-  def guess(actual, range, value \\ 500)
+  def guess(actual, a..b) do
+    guess(actual, a..b, div(b-a+1, 2))
+  end
 
   def guess(actual, _, actual) do
     IO.puts "Is it #{actual}"
     IO.puts "#{actual}"
   end
 
-  def guess(actual, range, value) when value < actual do
+  def guess(actual, _..b, value) when value < actual do
     IO.puts "Is it #{value}"
-    _..b = range
     guess(actual, value+1..b, value+div(b-value+1, 2))
   end
 
-  def guess(actual, range, value) when actual < value do
+  def guess(actual, a.._, value) when actual < value do
     IO.puts "Is it #{value}"
-    a.._ = range
     guess(actual, a..value-1, a+div(value-1-a, 2))
   end
 end
