ae64857795d375965a3384d888e6eaebdad98d9b
mitsu9 <mitsunobu.h.9@gmail.com>
2017-07-05 14:27:41 +0900

    Add max

--- mylist.exs
+++ mylist.exs
@@ -3,6 +3,14 @@ defmodule MyList do
 
   defp _mapsum([], _func, value), do: value
   defp _mapsum([head | tail], func, value), do: _mapsum(tail, func, value + func.(head))
+
+  # max(list)
+  def max([head | tail]), do: _max(tail, head)
+
+  defp _max([], max), do: max
+  defp _max([head | tail], max) when head < max, do: _max(tail, max)
+  defp _max([head | tail], max) when max < head, do: _max(tail, head)
 end
 
 IO.puts MyList.mapsum([1, 2, 3], &(&1 * &1))
+IO.puts MyList.max([1, 5, 7, 2, 4])
