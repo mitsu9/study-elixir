5896ba24f68556c130df92093e73bf93b9aa1068
mitsu9 <mitsunobu.h.9@gmail.com>
2017-07-05 14:42:25 +0900

    Add ceasar

--- mylist.exs
+++ mylist.exs
@@ -10,7 +10,13 @@ defmodule MyList do
   defp _max([], max), do: max
   defp _max([head | tail], max) when head < max, do: _max(tail, max)
   defp _max([head | tail], max) when max < head, do: _max(tail, head)
+
+  # ceasar(list, n)
+  def ceasar([], _n), do: []
+  def ceasar([head | tail], n) when 122 < head+n, do: [head+n-26, ceasar(tail, n)]
+  def ceasar([head | tail], n), do: [head+n, ceasar(tail, n)]
 end
 
 IO.puts MyList.mapsum([1, 2, 3], &(&1 * &1))
 IO.puts MyList.max([1, 5, 7, 2, 4])
+IO.puts MyList.ceasar('ryvkve', 13)
