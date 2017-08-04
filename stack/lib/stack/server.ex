2d5cd649bd16962f4af343073700c044a53566fe
mitsu9 <mitsunobu.h.9@gmail.com>
2017-08-04 17:52:08 +0900

    Add push method to stack

--- stack/lib/stack/server.ex
+++ stack/lib/stack/server.ex
@@ -4,4 +4,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail }
   end
+
+  def handle_cast({:push, v}, current_list) do
+    { :noreply, current_list ++ [v] }
+  end
 end
