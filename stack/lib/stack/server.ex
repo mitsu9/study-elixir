2d0dbb6f18181531403af99f89869774ea17d74e
mitsu9 <mitsunobu.h.9@gmail.com>
2017-08-04 18:33:56 +0900

    Add api to stack

--- stack/lib/stack/server.ex
+++ stack/lib/stack/server.ex
@@ -1,6 +1,24 @@
 defmodule Stack.Server do
   use GenServer
 
+  #####
+  # 外部API
+
+  def start_link(list) do
+    GenServer.start_link(__MODULE__, list, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def push(item) do
+    GenServer.cast __MODULE__, {:push, item}
+  end
+
+  #####
+  # GenServerの実装
+
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail }
   end
